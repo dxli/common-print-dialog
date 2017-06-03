5242f9cb04471dceb63a0cd8d92f6fb932966a97
Dongxu Li <dli@tamaggo.com>
2017-06-02 23:14:26 -0400

    Query the device pixel ratio

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QScreen>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +10,9 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+
+    // example to query the device pixel ratio
+    qDebug()<<"devicePixelRatio = "<<QGuiApplication::primaryScreen()->devicePixelRatio();
 
     return app.exec();
 }
